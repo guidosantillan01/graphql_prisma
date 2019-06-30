bb62cf77905a72391a767d67c5665beb66f2a778
Guido Santillan <guidosantillan01@gmail.com>
2019-06-30 14:30:10 -0500

    Locking down individual type fields

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,3 +1,21 @@
-const User = {};
+import getUserId from '../utils/getUserId.js';
+
+const User = {
+  email(parent, args, { request }, info) {
+    const userId = getUserId(request, false);
+
+    // parent fields might not be available. E.g. if we do not call the id in the query, the condition
+    // below is false.
+    if (userId && userId === parent.id) {
+      return parent.email;
+    } else {
+      return null;
+    }
+  },
+  password(parent, args, ctx, info) {
+    // Security flaw, where the password returns when asked. It should not do that.
+    return null;
+  }
+};
 
 export { User as default };
